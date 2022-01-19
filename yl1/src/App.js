43632f0ab60f9bf213d3bc57b80fc8c4eddc9145
Vivvup <viiviane@hotmail.com>
2022-01-19 14:06:08 +0200

    Esimene yl

---
+++ yl1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   
+       <button className= "buttonStyle">Vaata</button><br/>
+       <text className= "tekst">Sinine tekst</text><br/>
+       <img src="https://uudis.net/media/6/5/6/1613656.jpg" alt= "Ahja"/>
+      
+    </div>
+  );
+}
+
+export default App;
